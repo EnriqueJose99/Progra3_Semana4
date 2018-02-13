4dfb95c167ddbf5af2028b0b39f5558777ee9aa0
EnriqueJose99 <enrique.galeano99@yahoo.com>
2018-02-13 08:07:24 -0600

    Clases objetos

---
+++ MiClase.h
@@ -0,0 +1,31 @@
+#ifndef MICLASE_H
+#define MICLASE_H
+#include <string>
+	
+using namespace std;
+class MiClase{ 
+	//atributos o metodos privados
+	private:
+		int i;
+		string nombre;
+	//atributo o metodos publicos
+	public:
+		//prototipo de metodos
+		//constructor sobrecargado
+		MiClase(string, int);
+		//constructor por defecto
+		MiClase();
+		//mutadores
+		int getCodigo();
+		void setCodigo(int);
+		string getNombre();
+		void setNombre(string);
+		//destructor
+		//es publico, mismo nombre de la clase, no lleva parametros, solo
+		//puede haber uno
+		~MiClase();
+};
+
+#endif
+//Directivas de preprocesador:w
+
--- pOO.cpp
+++ pOO.cpp
@@ -1,1 +1,35 @@
+#include "MiClase.h"
 
+#include <string>
+using namespace std;
+
+MiClase::MiClase(string pNombre, int pCodigo){
+	nombre = pNombre;
+	codigo = pCodigo;  
+}
+
+MiClase::MiClase(){
+	nombre = "Juan Luis xd";
+	codigo = 504;
+}
+
+int MiClase::getCodigo(){
+	return codigo;
+}
+void MiClase::setCodigo(int pCodigo){
+	codigo = pCodigo;
+}
+
+string MiClase::getNombre(){
+	return nombre;
+}
+
+void MiClase::setNombre(string pNombre){
+	nombre = pNombre;
+}
+
+
+/*destructor*/
+~MiClase::~MiClase(){
+	cout<<"La instancia fue eliminada"<<endl;
+}
